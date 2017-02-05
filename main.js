f170ff6d8aa694b9012d261a5356638faa624bc8
nullpos <Greenstar77@gmail.com>
2017-02-05 14:52:25 +0900

    add script

---
+++ main.js
@@ -0,0 +1,59 @@
+var page = require('webpage').create();
+
+page.open('https://mobile.twitter.com/login', function(status) {
+    if(status !== "success") {
+        console.log("Unable to load url.");
+        phantom.exit();
+    }
+    page.render("1.png");
+    console.log('login...');
+    page.evaluate(function() {
+        document.getElementById("session[username_or_email]").value = "";
+        document.getElementById("session[password]").value = "";
+        document.forms[0].submit();
+    });
+    page.onLoadFinished = logined
+});
+
+function logined() {
+    console.log('logined.');
+    page.onLoadFinished = null;
+    page.open('https://mobile.twitter.com/follower_requests', function(status) {
+        if(status !== "success") {
+            console.log("Unable to load url.");
+            phantom.exit();
+        }
+        console.log('fetch follower requests');
+        page.render("2.png");
+        var forms = page.evaluate(function() {
+            return document.forms;
+        });
+        if(forms[0]) {
+            console.log('requests found');
+            approve();
+        } else {
+            console.log('no follower requests found');
+            phantom.exit();
+        }
+    });
+}
+
+function approve() {
+    var flag = page.evaluate(function() {
+        if(!document.forms[0] || document.forms[0].action.indexOf("search") !== -1) {
+            return false;
+        } else {
+            var ref = document.getElementsByClassName("fullname")[0].parentNode.href;
+            document.forms[0].submit();
+            return ref.replace(/.*\/(.*?)\?.*/, "$1");
+        }
+    });
+    if(flag) {
+        console.log('approve: ' + flag);
+        page.onLoadFinished = approve;
+    } else {
+        page.onLoadFinished = null;
+        console.log('approved requests');
+        phantom.exit();
+    }
+}
