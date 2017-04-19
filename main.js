db3c6921e5ae2be6470f02b7bade989032bab418
nullpos <greenstar77@gmail.com>
2017-04-20 06:42:01 +0900

    add login failed process

--- main.js
+++ main.js
@@ -5,32 +5,40 @@ page.open('https://mobile.twitter.com/login', function(status) {
         console.log("Unable to load url.");
         phantom.exit();
     }
-    console.log('login...');
+    console.log('Now login...');
     page.evaluate(function() {
         document.getElementById("session[username_or_email]").value = "";
         document.getElementById("session[password]").value = "";
         document.forms[0].submit();
     });
+    var is_logged_in = page.evaluate(function() {
+        return document.getElementById("session[username_or_email]") == null;
+    });
+    if(!is_logged_in) {
+        console.log('Unable to login. Please check username, email, password.');
+        phantom.exit();
+    }
     page.onLoadFinished = loggedIn
 });
 
 function loggedIn() {
-    console.log('logged in.');
+    console.log('Logged in.');
+
     page.onLoadFinished = null;
     page.open('https://mobile.twitter.com/follower_requests', function(status) {
         if(status !== "success") {
-            console.log("Unable to load url.");
+            console.log("Unable to load follower requests page.");
             phantom.exit();
         }
-        console.log('fetch follower requests');
+        console.log('Fetching follower requests.');
         var forms = page.evaluate(function() {
             return document.forms;
         });
         if(forms[0]) {
-            console.log('requests found');
+            console.log('Requests found!');
             approve();
         } else {
-            console.log('no follower requests found');
+            console.log('No follower requests found.');
             phantom.exit();
         }
     });
@@ -47,11 +55,11 @@ function approve() {
         }
     });
     if(flag) {
-        console.log('approve: ' + flag);
+        console.log('Approve: ' + flag);
         page.onLoadFinished = approve;
     } else {
         page.onLoadFinished = null;
-        console.log('approved requests');
+        console.log('Approved requests.');
         phantom.exit();
     }
 }
